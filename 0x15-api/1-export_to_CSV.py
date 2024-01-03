8a51ef6cbbc0df8c2380b8ad917784594d977d1c
dimka90 <dimkayilrit@gmail.com>
2024-01-03 13:42:24 +0100

    added a function that get the username of the user

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""This module a Rest Api that fetches information from the REST API"""
+import requests
+import sys
+from sys import argv
+
+
+def Userinfor(user_id):
+    """
+    Fetches user and todolist from REST API and extract some information
+    Returns:
+    - None: None
+    """
+    task = []
+    count = 0
+    completed_task = 0
+    user_data_url = "https://jsonplaceholder.typicode.com/users"
+    todolist_data_url = "https://jsonplaceholder.typicode.com/todos"
+    try:
+        # sending a get request to the user url
+        user_response = requests.get(user_data_url)
+        # sending a get request to the todo list url
+        todolist_respons = requests.get(todolist_data_url)
+        # Raise Exception if anything gone bad
+        user_response.raise_for_status() or todolist_respons.raise_for_status()
+        # convert Response to Json objects
+        user_response_json = user_response.json()
+        # converting todolist to json objects
+        todo_response_json = todolist_respons.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching TODOs for employee {user_id}: {e}")
+        sys.exit(1)
+
+    # getting username 
+    for user in user_response_json:
+        if user.get("id") == user_id:
+            username = user.get("username")
+    # Searching for the user with specific id in the todo list
+    for todo in todo_response_json:
+        # checking for the UserId in the todo list
+        if todo.get("userId") == user_id:
+            task.append(todo)
+
+
+if __name__ == "__main__":
+    user_id = int(argv[1])
+    Userinfor(user_id)
