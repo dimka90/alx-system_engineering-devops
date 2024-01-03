b61a541e967e41089964a6dfc14e8ff0b8cf1f0f
dimka90 <dimkayilrit@gmail.com>
2024-01-03 07:08:32 +0100

    successfully created a connection to the restful api and fetches data to the remote machine

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 """This module a Rest Api that fetches information from the REST API"""
+import requests
+from sys import argv
+
+
+def Userinfor(user_id):
+    """
+    Fetches user and todolist from REST API and extract some informatin
+    
+    Returns:
+    - None: None
+    """
+
+    user_data = requests.get("https://jsonplaceholder.typicode.com/users")
+    print(user_data.text)
+
+    print(user_id)
+
+
+
+
+if __name__ =="__main__":
+    user_id = int(argv[1])
+    Userinfor(user_id)
